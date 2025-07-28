72444bd5de1e29943348aaf0862dc7c39d61f0e5
Tejas kadam <kadamtejas220@gmail.com>
2025-07-28 22:12:18 +0530

    Add files via upload

---
+++ C Module/ASSIGNMENT 0/average.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+	int average,num1=10,num2=20,num3=30,num4=40,num5=50,sum;
+	 
+	 average=(num1+num2+num3+num4+num5)/5;
+	 
+	 printf("average is =%d",average);
+}
