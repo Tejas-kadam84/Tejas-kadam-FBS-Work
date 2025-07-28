7ab426a7f76fa10ebc1e6fb5f03afcdc12bef440
Tejas kadam <kadamtejas220@gmail.com>
2025-07-28 22:16:15 +0530

    Add files via upload

---
+++ C Module/ASSIGNMENT 0/perimeter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	float length,width,perimeter;
+	 double len =10;
+	 double  wid =50;
+	 
+	 
+	 perimeter = 2 * (length + width);
+	 
+	 printf("%d",perimeter);
+}
