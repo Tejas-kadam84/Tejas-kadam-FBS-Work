7e8e486327a9ecc042bed68c62b52d2d635bae39
Tejas kadam <kadamtejas220@gmail.com>
2025-08-08 00:33:09 +0530

    Add files via upload

---
+++ C Module/Class Test 2/pnn.c
@@ -0,0 +1,23 @@
+//Que 2. WAP to check if number is positive, negative or neutral (0).
+#include<stdio.h>
+void main()
+{
+	int num;
+	
+	printf("enter the number:");
+	scanf("%d", &num);
+	
+	if(num>0)
+	{
+		printf("number is positive");
+	}
+	else if(num<0)
+	{
+		printf("number is negative");
+	}
+	else
+	{
+		printf("number is neutral");
+	}
+	
+}
